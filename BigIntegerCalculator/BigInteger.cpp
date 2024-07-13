bcf90f0e5f5cf1de22876a78f1778e8065738e7e
pavsethi <111523103+pavsethi@users.noreply.github.com>
2024-07-13 16:01:03 -0700

    Update BigInteger.cpp

--- BigIntegerCalculator/BigInteger.cpp
+++ BigIntegerCalculator/BigInteger.cpp
@@ -1,10 +1,3 @@
-/********************************************************************************* 
-* Pav Sethi, pssethi
-* 2023 Winter CSE101 PA #6
-* BigInteger.cpp
-* Implementation of BigInteger ADT 
-*********************************************************************************/ 
-
 #include<iostream>
 #include<string>
 #include<stdexcept>
