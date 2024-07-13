57661d57b60a3919e4cf994eafa31cb4d1385fcd
pavsethi <111523103+pavsethi@users.noreply.github.com>
2024-07-13 15:59:50 -0700

    Update Matrix.h

--- MatrixADT/Matrix.h
+++ MatrixADT/Matrix.h
@@ -1,10 +1,3 @@
-/********************************************************************************* 
-* Pav Sethi, pssethi
-* 2023 Winter CSE101 PA #4
-* Matrix.h
-* Header file for Matrix ADT 
-*********************************************************************************/ 
-
 #pragma once
 #include<stdbool.h>
 #include <string.h>
